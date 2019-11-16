591ca66876b12d6e62e3457925cab8ff5c070dd9
Mustofa Kamal <mustofa2008@gmail.com>
2019-11-16 15:13:40 +0600

    Initial commit

--- app/src/main/java/com/quickstart/vidyoiovideo/MainActivity.java
+++ app/src/main/java/com/quickstart/vidyoiovideo/MainActivity.java
@@ -1,40 +1,107 @@
 package com.quickstart.vidyoiovideo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
-import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.vidyo.VidyoClient.Connector.ConnectorPkg;
 import com.vidyo.VidyoClient.Connector.Connector;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity implements Connector.IConnect {
 
     private static final String TAG="MainActivity";
     private Connector vc;
     private FrameLayout videoFrame;
+    private String mToken;
+
+    //Runtime Permissions
+    private String[] PERMISSIONS = { android.Manifest.permission.CAMERA, android.Manifest.permission.RECORD_AUDIO };
+    private PermissionUtility mPermissions;
+    private MyNetworkReceiver mNetworkReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mNetworkReceiver = new MyNetworkReceiver(this);
+        mPermissions = new PermissionUtility(this, PERMISSIONS); //Runtime permissions
+
         ConnectorPkg.setApplicationUIContext(this);
         ConnectorPkg.initialize();
         videoFrame = (FrameLayout)findViewById(R.id.videoFrame);
+
+        getVidyoToken();
+
+        //Runtime Permissions
+        if(mPermissions.arePermissionsEnabled()){
+            vidyoStart();
+            Log.d(TAG, "Permission granted 1");
+        } else {
+            mPermissions.requestMultiplePermissions();
+        }
+
+
+
+
     }
 
-    public void Start(View v) {
-        vc = new Connector(videoFrame, Connector.ConnectorViewStyle.VIDYO_CONNECTORVIEWSTYLE_Default, 15, "warning info@VidyoClient info@VidyoConnector", "", 0);
-        vc.showViewAt(videoFrame, 0, 0, videoFrame.getWidth(), videoFrame.getHeight());
+    //Runtime permissions
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(mPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            vidyoStart();
+            Log.d(TAG, "Permission granted 2");
+        }
+    }
+
+    //===============================================| onStart(), onPause(), onResume(), onStop()
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(mNetworkReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        try {
+            unregisterReceiver(mNetworkReceiver);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void vidyoStart() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                vc = new Connector(videoFrame, Connector.ConnectorViewStyle.VIDYO_CONNECTORVIEWSTYLE_Default, 15, "warning info@VidyoClient info@VidyoConnector", "", 0);
+                vc.showViewAt(videoFrame, 0, 0, videoFrame.getWidth(), videoFrame.getHeight());
+            }
+        }, 3000);
     }
 
     public void Connect(View v) {
-        String token = "cHJvdmlzaW9uAERlbW9Vc2VyQDAzOWQxNC52aWR5by5pbwA2Mzc0MDI1ODA0NwAAODQ4YmI3OWJkNTRmNWU0YzVhZDY4M2I1ZTQwYWU5M2FjOGExOTcxNGMyYTNkMDRjZmIxYjY3OGYwZmRhOWE2ZGJiYTIxM2Q4ZDAxYWI3OTE4MTJiZmQxYWMxODI5YmZk";
-        vc.connect("prod.vidyo.io", token, "DemoUser", "DemoRoom", this);
+        if (mToken != null) {
+            vc.connect("prod.vidyo.io", mToken, "DemoUser", "DemoRoom", this);
+        }
     }
 
     public void Disconnect(View v) {
@@ -51,5 +118,29 @@ public class MainActivity extends AppCompatActivity implements Connector.IConnec
 
     public void onDisconnected(Connector.ConnectorDisconnectReason reason) {
         Log.d(TAG, "ConnectorDisconnectReason "+reason.name());
+    }
+
+    public void getVidyoToken() {
+        String url = "https://us-central1-vidyoio.cloudfunctions.net/getVidyoToken";
+        StringRequest stringRequest = new StringRequest(url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    mToken = jsonObject.getString("token");
+                    Log.d(TAG, mToken);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Log.e(TAG, e.getMessage());
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, error.getMessage());
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 }
